e835b8fc08d0f25e29a5c04935363a65c8f556db
Brian <brian.nghiem@mail.utoronto.ca>
2025-03-18 12:47:08 -0400

    fixed typo in line 9, end of x_batch definition was missing closing ] bracket...did they not test this code pre-release?

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -6,7 +6,7 @@ def data_generator(input_imgs, target_imgs, batch_size=1):
     for batch_num in range(0, num_images, batch_size):
         i = batch_num
         i_end = i + batch_size
-        x_batch=np.array(input_imgs[i:i_end,:,:,:,:)
+        x_batch=np.array(input_imgs[i:i_end,:,:,:,:])
         y_batch=np.array(target_imgs[i:i_end,:,:,:,:])
                                                 
         #print(x_batch.shape) 
